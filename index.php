57892175cb9339d408c175b84a69c98a56cbc984
giustoantonadev <giustoantonadev@gmail.com>
2026-06-09 17:10:31 +0200

    Add password generator functionality with user input for length

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+$passwordLength = isset($_GET['length']) ? (int)$_GET['length'] : 0;
+
+function generatePassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $password;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    <h1>Password Generator</h1>
+    <form action="index.php" method="get">
+        <label for="length">Password Length:</label>
+        <input type="number" id="length" name="length" min="1" max="10" required>
+        <br><br>
+        <input type="submit" value="Generate Password">
+    </form>
+    <?php
+    if ($passwordLength > 0) {
+        echo "<p>Generated Password: " . htmlspecialchars(generatePassword($passwordLength)) . "</p>";
+    }
+    ?>
+</body>
+</html>
