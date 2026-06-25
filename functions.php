d8ac1024b6cc2dd3a8a478ddd4e6b79f8499f87d
giustoantonadev <giustoantonadev@gmail.com>
2026-06-25 14:17:54 +0200

    Enhance password generation by adding character set options and updating form handling

--- functions.php
+++ functions.php
@@ -1,10 +1,33 @@
 <?php
-function generatePassword($length)
+function generatePassword($length, $options)
 {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
-    $password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, strlen($characters) - 1)];
+
+    $characters = '';
+
+    if ($options['lowercase']) {
+        $characters .= 'abcdefghijklmnopqrstuvwxyz';
     }
+    if ($options['uppercase']) {
+        $characters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if ($options['numbers']) {
+        $characters .= '0123456789';
+    }
+    if ($options['symbols']) {
+        $characters .= '!@#$%^&*()';
+    }
+
+    // Se nessun set è selezionato → errore
+    if ($characters === '') {
+        return 'Errore: seleziona almeno un set di caratteri.';
+    }
+
+    $password = '';
+    $maxIndex = strlen($characters) - 1;
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, $maxIndex)];
+    }
+
     return $password;
 }
--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php
-
 session_start();
-
 require_once 'functions.php';
 
 $passwordLength = isset($_GET['length']) ? (int)$_GET['length'] : 0;
 
-if ($passwordLength >  0) {
-    $generatedPassword = generatePassword($passwordLength);
+if ($passwordLength > 0) {
 
+    // Costruisco l’array dei set selezionati
+    $options = [
+        'lowercase' => isset($_GET['lowercase']),
+        'uppercase' => isset($_GET['uppercase']),
+        'numbers'   => isset($_GET['numbers']),
+        'symbols'   => isset($_GET['symbols'])
+    ];
+
+    // Genero la password
+    $generatedPassword = generatePassword($passwordLength, $options);
+
+    // Salvo in sessione
     $_SESSION['generated_password'] = $generatedPassword;
 
     header('Location: result.php');
     exit();
 }
-
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,6 +40,18 @@ if ($passwordLength >  0) {
     <form action="index.php" method="get">
         <label for="length">Password Length:</label>
         <input type="number" id="length" name="length" min="1" max="10" required>
+        <br><br>
+        <input type="checkbox" id="lowercase" name="lowercase" checked>
+        <label for="lowercase">Include Lowercase</label>
+        <br>
+        <input type="checkbox" id="uppercase" name="uppercase" checked>
+        <label for="uppercase">Include Uppercase</label>
+        <br>
+        <input type="checkbox" id="numbers" name="numbers" checked>
+        <label for="numbers">Include Numbers</label>
+        <br>
+        <input type="checkbox" id="symbols" name="symbols" checked>
+        <label for="symbols">Include Symbols</label>
         <br><br>
         <input type="submit" value="Generate Password">
     </form>
